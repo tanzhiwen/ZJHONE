dcbec0fd3d8165c2a347c95c7b47a01a39b095e7
Miracle_Tan <1054253786@qq.com>
2018-06-14 14:44:58 +0800

    commited: begin

---
+++ ZJH/ZJH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJH
+//
+//  Created by 檀志文 on 2018/6/14.
+//  Copyright © 2018年 檀志文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
